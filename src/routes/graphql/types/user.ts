c0c4e7cc4899b7d0d8678e8168d00c49823f9166
Aleksei Fedorenko <fedorenksei@gmail.com>
2024-03-05 11:46:03 +0400

    feat: implement user subscribers and subscriptions

--- src/routes/graphql/types/user.ts
+++ src/routes/graphql/types/user.ts
@@ -4,9 +4,9 @@ import { Post } from './post.js';
 import { UUIDType } from './uuid.js';
 import { Profile } from './profile.js';
 
-export const User = new GraphQLObjectType({
+export const User: GraphQLObjectType = new GraphQLObjectType({
   name: 'User',
-  fields: {
+  fields: () => ({
     id: { type: UUIDType },
     name: { type: GraphQLString },
     balance: { type: GraphQLFloat },
@@ -22,5 +22,21 @@ export const User = new GraphQLObjectType({
         return prisma.user.findUnique({ where: { id: source.id } }).profile();
       },
     },
-  },
+    userSubscribedTo: {
+      type: new GraphQLList(User),
+      resolve(source: { id: string }, args, { prisma }: Context) {
+        return prisma.user.findMany({
+          where: { subscribedToUser: { some: { subscriberId: source.id } } },
+        });
+      },
+    },
+    subscribedToUser: {
+      type: new GraphQLList(User),
+      resolve(source: { id: string }, args, { prisma }: Context) {
+        return prisma.user.findMany({
+          where: { userSubscribedTo: { some: { authorId: source.id } } },
+        });
+      },
+    },
+  }),
 });
